6837b2e4ed6dd83bde7b74a14023440dc494005f
luqman <luqmantarukhel@gmail.com>
2023-12-22 23:38:11 +0500

    Condition navigator And  Go Back button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  //condition base navigation
+  // const NavigateFunction = () => {
+  //   navigate("/contact");
+  // };
+
+  //? ______________________________________________________
+  //Navigate To Multiple Component with  single eventHandler
+  const NavigateAll = (url) => {
+    navigate(url);
+  };
+
+  //? ______________________________________________________
+  //navigate through condition
+  // const NavigateToWhere = () => {
+  //   let name = "waqas";
+  //   if (name == "waqas") {
+  //     navigate("/about");
+  //   } else {
+  //     navigate("/contact");
+  //   }
+  // };
+
   return (
     <>
       <Navbar />
@@ -15,6 +39,46 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      {/* <button
+        className="bg-blue-700 flex  gap-2 justify-between m-2 px-3 rounded-md text-white"
+        onClick={() => navigate("/about")}
+      >
+        About
+      </button> */}
+      {/* <button
+        className="bg-blue-700 flex  gap-2 justify-between m-2 px-3 rounded-md text-white"
+        onClick={() => navigate("/contact")}
+      >
+        Contact
+      </button> */}
+      {/* <button
+        className="bg-blue-700 flex  gap-2 justify-between m-2 px-3 rounded-md text-white"
+        onClick={() => NavigateFunction()}
+      >
+        Contact
+      </button> */}
+      {/* //?______________________________________________________ */}
+      <button
+        className="bg-blue-700 flex  gap-2 justify-between m-2 px-3 rounded-md text-white"
+        onClick={() => NavigateAll("/about")}
+      >
+        About
+      </button>
+      <button
+        className="bg-blue-700 flex  gap-2 justify-between m-2 px-3 rounded-md text-white"
+        onClick={() => NavigateAll("/contact")}
+      >
+        Contact
+      </button>
+      {/* //?________________________ Navigate Through Condition ______________________________ */}
+      {/* <button
+        className="bg-blue-700 flex  gap-2 justify-between m-2 px-3 rounded-md text-white"
+        onClick={() => NavigateToWhere()}
+      >
+        Click Me
+      </button> */}
+
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </>
   );
 };
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+// import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  // const navigate = useNavigate();
+
   return (
     <div>
       <h1 className="flex justify-center items-center h-screen font-serif font-extrabold ">
         About page
       </h1>
+      {/* <button onClick={() => navigate(-1)}>Go Back</button> */}
     </div>
   );
 };
